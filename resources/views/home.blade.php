eca7a93766c62bb7c6d3036e8484e06bf75eae77
Saud Naif <saudroyalpc@gmail.com>
2024-01-25 08:13:50 +0300

    adding animation and meta data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="description" content="Lurking Skull is an indie game studio based in Saudi Arabia, specializing in creating captivating horror games. With a focus on immersive storytelling and atmospheric gameplay, the studio crafts unique and bone-chilling experiences that will leave players on the edge of their seats. Delve into the depths of terror and prepare for unforgettable nightmares with Lurking Skull, the Saudi indie game studio that brings fear to life.">
     <title>Lurking Skull</title>
 
     <!-- Fonts -->
@@ -19,7 +19,7 @@
 
 <body class="font-sans antialiased bg-stone-950 bg-gif-background">
     <div class="flex items-center justify-center h-screen">
-        <div class="text-white text-center">
+        <div class="text-white text-center animate__animated animate__fadeInDown">
             <h1 class="text-5xl mb-6">Coming Soon...</h1>
             <h2 class="text-4xl mb-4">Follow Us</h2>
             <a href="https://twitter.com/Lurking_Skull"><i class="fa-brands fa-x-twitter text-3xl"></i></a>
